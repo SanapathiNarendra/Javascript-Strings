e22dd9dd168b81607593588e88619e9fdea8f933
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-04-03 18:58:23 +0530

    Feat(StringCaseConvert):Change the Name to UpperCase

--- StringCaseConvert.js
+++ StringCaseConvert.js
@@ -1,10 +1,19 @@
 document.getElementById("btnUpperCase").addEventListener("click",function(){
+    debugger;
     var name=document.getElementById("txtName").value;
     document.getElementById("pResult").innerHTML=name.toUpperCase();
 
 });
 document.getElementById("btnLowerCase").addEventListener("click",function(){
+    debugger;
     var name=document.getElementById("txtName").value;
     document.getElementById("pResult").innerHTML=name.toLowerCase();
+    
+});
+
+document.getElementById("btnTrim").addEventListener("click",function(){
+    debugger;
+    var name=document.getElementById("txtName").value;
+    document.getElementById("pResult").innerHTML=name.trim();
     
 });
