4cd900ab2b307b5d23f6dab09bf2e2e62fdcf3d9
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-06-03 16:47:10 +0530

    Feat(CountVowels):Implement the js function

---
+++ CountVowels.js
@@ -0,0 +1,21 @@
+function countVowels() {
+    var input = document.getElementById("word").value;
+    var count = 0;
+
+    for (let i = 0; i < input.length; i++) {
+        if (
+            input[i] == 'a' ||
+
+                input[i] == 'e' ||
+
+                input[i] == 'i' ||
+
+                input[i] == 'o' ||
+
+                input[i] == 'u') {
+            count++;
+        }
+    }
+    document.getElementById("output").innerHTML = "Number of vowels: " + count;
+
+}
