7ea8c9875d6b2b458cce4491ef336364ebc4e210
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-06-09 15:25:09 +0530

    Feat(StringReverse):Implement the javaScript

--- stringReverse.js
+++ stringReverse.js
@@ -5,5 +5,6 @@ function reverseString() {
     for (let i = string.length - 1; i >= 0; i--) {
         stringReverse = stringReverse + string[i];
     }
+    
     document.getElementById("pResult").innerHTML = stringReverse.toUpperCase();
 }
