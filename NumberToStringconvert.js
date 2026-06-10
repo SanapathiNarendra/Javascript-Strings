73332c8406b5d603d26b3a9cc22d9b0e52eec4d0
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-06-10 14:42:08 +0530

    Feat(NumberToStringConvert):Add a javaScript Code

--- NumberToStringconvert.js
+++ NumberToStringconvert.js
@@ -1,1 +1,6 @@
 
+       function convertNumberToString() {
+           var number = document.getElementById("numberInput").value;
+           var string = number.toString();
+           document.getElementById("result").innerText = "Converted string: " + string;
+       }
