cb2d984e97a6688adc90d4f82dd5da8cf0fea79b
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-06-12 19:01:25 +0530

    Implement palindrome number check using while loop

---
+++ palindrome.js
@@ -0,0 +1,22 @@
+function checkPalindrome() {
+
+    var number = Number(document.getElementById("txtNumber").value);
+
+    var originalNumber = number;
+    var reversedNumber = 0;
+
+    while (number > 0) {
+        var digit = number % 10;
+        reversedNumber = reversedNumber * 10 + digit;
+        number = Math.floor(number / 10);
+    }
+
+    if (originalNumber === reversedNumber) {
+        document.getElementById("pResult").innerHTML =
+            originalNumber + " is a Palindrome";
+    }
+    else {
+        document.getElementById("pResult").innerHTML =
+            originalNumber + " is Not a Palindrome";
+    }
+}
