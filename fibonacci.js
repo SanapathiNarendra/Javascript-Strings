d74fa80c0b17cdc92cca85ca56916dfcd31378c0
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-06-13 23:32:19 +0530

    Implement Fibonacci series generation using for loop

---
+++ fibonacci.js
@@ -0,0 +1,19 @@
+function generateFibonacci() {
+
+    var terms = Number(document.getElementById("txtNumber").value);
+
+    var first = 0;
+    var second = 1;
+    var series = "";
+
+    for (var i = 1; i <= terms; i++) {
+        series += first + " ";
+
+        var next = first + second;
+        first = second;
+        second = next;
+    }
+
+    document.getElementById("pResult").innerHTML =
+        "Fibonacci Series: " + series;
+}
