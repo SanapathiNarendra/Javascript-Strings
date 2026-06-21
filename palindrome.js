722c9eb920553329dce3f921c543690f132ced01
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-06-21 23:04:34 +0530

    Feat(Palindrome):Implement the js function

--- palindrome.js
+++ palindrome.js
@@ -8,6 +8,7 @@ function checkPalindrome() {
     while (number > 0) {
         var digit = number % 10;
         reversedNumber = reversedNumber * 10 + digit;
+        
         number = Math.floor(number / 10);
     }
 
