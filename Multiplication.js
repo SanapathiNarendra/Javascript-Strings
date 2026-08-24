8eab1edb6fed68a89e232a95e27200754854414f
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-08-24 18:21:59 +0530

    Feat(Multiplication):Implement the js Code

--- Multiplication.js
+++ Multiplication.js
@@ -4,5 +4,4 @@ function multiply(num1, num2) {
 }
 var num1 = 5;
 var num2 = 3;
-
 var result = alert( multiply(num1, num2));
--- StringAddition.js
+++ StringAddition.js
@@ -1,6 +1,10 @@
-function addStrings() {    
+function addStrings() {  
+    debugger;  
     var str1 = "Hello, ";
     var str2 = "World!";
     var result = str1 + str2;
-    console.log(result);
+    alert(result);
+
 }
+
+addStrings();
